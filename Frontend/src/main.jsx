2c8b172aa368c75f8edcc3dd6bd7ecf5ad5e8c84
Mehtab Singh <mehtabsinghcse@gmail.com>
2024-09-17 03:48:18 +0530

    Removal: React.StrictMode is removed from the main.jsx file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,10 +7,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <BrowserRouter>
       <App className="w-full" />
       <ToastContainer />
     </BrowserRouter>
-  </React.StrictMode>,
 )
